7c5eec2378231f66beab8169cc924e5c81e9e7e2
Yogesh Chandra Singh Samant <yogeshchsamant@gmail.com>
2024-04-08 22:44:06 +0530

    work 1

--- portfolio/src/components/Skills.jsx
+++ portfolio/src/components/Skills.jsx
@@ -17,7 +17,7 @@ const Skills = () => {
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
           <p className="text-4xl font-bold inline border-b-4 border-pink-600">
-            Experience
+            Skills
           </p>
           <p className="py-4">//These are the tech I've worked with//</p>
         </div>
---
+++ portfolio/src/components/Work.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import WorkImg from "../assets/projects/workImg.jpeg";
+import RealEstateImg from "../assets/projects/realestate.jpg";
+
+const Work = () => {
+  return (
+    <div name="work" className="w-full md:h-screen bg-[#0a192f] text-gray-300">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+            Work
+          </p>
+          <p className="py-6">//Check out my work//</p>
+        </div>
+
+        {/* grid container */}
+        <div
+          style={{ backgroundImage: `url(${WorkImg})` }}
+          className="grid px-4 sm:grid-cols-2 md:grid-cols-3 gap-4"
+        >
+          {/* card container */}
+          <div className="shadow-lg shadow-gray-900 group container rounded-md flex justify-center items-center mx-auto content-div">
+            {/* hover effect */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wide">
+                ReactJS App
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
