90ba0ab19e5811be80f570b5e6cf8def2006a6e7
Yogesh Chandra Singh Samant <yogeshchsamant@gmail.com>
2024-04-08 23:56:48 +0530

    work 2

--- portfolio/src/components/Work.jsx
+++ portfolio/src/components/Work.jsx
@@ -14,12 +14,11 @@ const Work = () => {
         </div>
 
         {/* grid container */}
-        <div
-          style={{ backgroundImage: `url(${WorkImg})` }}
-          className="grid px-4 sm:grid-cols-2 md:grid-cols-3 gap-4"
-        >
-          {/* card container */}
-          <div className="shadow-lg shadow-gray-900 group container rounded-md flex justify-center items-center mx-auto content-div">
+        <div className="grid px-4 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          
+          {/* card container / grid item */}
+          <div style={{ backgroundImage: `url(${WorkImg})` }}  className="shadow-lg shadow-gray-900 group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
             {/* hover effect */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wide">
@@ -27,10 +26,14 @@ const Work = () => {
               </span>
               <div className="pt-8 text-center">
                 <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
                 </a>
                 <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
                 </a>
               </div>
             </div>
