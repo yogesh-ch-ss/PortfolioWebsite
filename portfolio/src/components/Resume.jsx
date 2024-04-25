1deb19fe51cbec0f04121eeaf9a5301d453916c6
Yogesh Chandra Singh Samant <yogeshchsamant@gmail.com>
2024-04-25 21:28:05 +0530

    resume background video

--- portfolio/src/components/Resume.jsx
+++ portfolio/src/components/Resume.jsx
@@ -2,20 +2,25 @@ import React from "react";
 import { BsFileEarmarkText } from "react-icons/bs";
 import { FaAngleRight } from "react-icons/fa6";
 
+import aurora from "../assets/aurora.mp4";
+
 const Resume = () => {
   return (
     <a href="#">
-      <div
-        name="resume"
-        // className="group w-full h-[20rem] bg-gradient-to-br from-pink-600 via-fuchsia-800 to-violet-900 from-10% via-50% px-20"
-        // className="group w-full h-[20rem] px-20 bg-gradient-to-br from-pink-900 via-purple-900 to-blue-900"
-        className="group w-full h-[20rem] px-20 bg-gradient-to-br from-purple-900 to-red-900"
-      >
-        {/* <div
-        name="resume"
-        className="group w-full h-[20rem] bg-gradient-to-t from-teal-800 to-teal-700 px-20"
-      > */}
-        <div className="flex flex-col justify-center items-center h-full text-neutral-100">
+      <div name="resume" className="relative group w-full h-[20rem] px-20">
+        {/* Video Background */}
+        <video
+          src={aurora}
+          autoPlay
+          loop
+          muted
+          className="absolute inset-0 object-cover w-full h-full"
+        />
+
+        {/* Dim Overlay */}
+        <div className="absolute inset-0 bg-black opacity-50" />
+
+        <div className="absolute inset-0 flex flex-col justify-center items-center h-full text-neutral-100">
           <div className="px-4 py-4 text-[7rem] duration-200">
             <BsFileEarmarkText />
           </div>
